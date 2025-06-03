f4f69a76117f15af587c35e2666ca64e08381252
pixelman77 <haghgoo33@gmail.com>
2025-06-03 22:53:38 +0330

    Update Database.php

--- core/Database.php
+++ core/Database.php
@@ -6,7 +6,7 @@ use PDO;
 
 class Database {
     public static function connect(): PDO {
-        return new PDO('mysql:host=localhost;dbname=web_final', 'root', '', [
+        return new PDO('mysql:host=127.0.0.1;port=3306;dbname=web_final', 'root', 'root', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
     }
